447d5cf4b3a2bb84c4777094606aa3793a4b7097
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-03-29 18:10:46 -0300

    Preparing the env and installing fastAPI

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Engenharia da Computação!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def create_item(item: dict):
+    return {"item": item}
